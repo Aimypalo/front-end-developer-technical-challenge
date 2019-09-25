9ebfaf5a118f8db74c248c6d12e9da885239f0a5
Aimy Palogiannidou <emmapalo@gmail.com>
2019-09-25 15:00:31 +0100

    fixed bugs and added missing blocks

--- content/js/card.js
+++ content/js/card.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     objectFitImages();
 });
+
+jQuery(document).ready(function($){
+
+	// Favorite button
+	$('.favorite').on('click', function(event){
+		event.preventDefault();
+
+		$(this).toggleClass('enabled');
+	});
+
+});
